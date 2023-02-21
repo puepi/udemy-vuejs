4399355964836c96301baab9dffd42a2c1b06a26
karoud <pmandel0001@yahoo.fr>
2023-02-21 23:01:02 +0100

    coachFilter component implemented

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,5 +1,5 @@
 <template>
-    <coach-filter></coach-filter>
+    <coach-filter @change-filter="setFilters"></coach-filter>
         <base-card>
             <div class="controls">
                 <base-button mode="outline">Refresh</base-button>
@@ -20,14 +20,40 @@
     import CoachFilter from '../../components/coaches/CoachFilter.vue'
     export default{
         components:{CoachItem,CoachFilter},
+        data(){
+            return{
+                activeFilters:{
+                    frontend:true,
+                    backend:true,
+                    career:true
+                }
+            }
+        },
         computed:{
             filteredCoaches(){
-                return this.$store.getters['coaches/coaches']
+                const coaches= this.$store.getters['coaches/coaches']
+                return coaches.filter(coach=>{
+                    if(this.activeFilters.frontend && coach.areas.includes('frontend')){
+                        return true
+                    }
+                    if(this.activeFilters.backend && coach.areas.includes('backend')){
+                        return true
+                    }
+                    if(this.activeFilters.career && coach.areas.includes('career')){
+                        return true
+                    }
+                    return false
+                })
             },
             hasCoaches(){
                 return this.$store.getters['coaches/hasCoaches']
             }
         },
+        methods:{
+            setFilters(updatedFilters){
+                this.activeFilters=updatedFilters
+            }
+        }
     }
 </script>
 <style scoped>
